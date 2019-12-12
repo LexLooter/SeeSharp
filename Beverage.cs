71e7b1e9dba4abe1730f454499aa213e3aa2be7c
LexLooter <patrick.luther@outlook.com>
2019-12-11 19:13:20 -0500

    Add project files.

---
+++ Beverage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeeSharp
+{
+    public abstract class Beverage
+    {
+
+    }
+
+    public class Beer : Beverage
+    {
+
+    }
+}
